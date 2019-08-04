8c4edac73f752b76e73e921d807d44ab097fd1da
NITIN KUMAR SINGH <nitinitleen1@gmail.com>
2019-08-04 17:02:22 +0530

    getting user data,signup logic added

--- __init__.py
+++ __init__.py
@@ -10,6 +10,7 @@ def create_app():
 
     app.config['SECRET_KEY'] = os.urandom(10)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
     
--- auth.py
+++ auth.py
@@ -1,7 +1,9 @@
 # auth.py
 
-from flask import Blueprint ,render_template
+from flask import Blueprint, render_template, redirect, url_for, request, flash
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from .model import User
 
 auth = Blueprint('auth', __name__)
 
@@ -12,6 +14,27 @@ def login():
 @auth.route('/signup')
 def signup():
     return render_template('signup.html')
+
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    # code to validate and add user to database goes here
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
+
+    if user:# if a user is found, we want to redirect back to signup page so user can try again
+        flash('Email address already exists')
+        return redirect(url_for('auth.signup'))
+
+    # create new user with the form data. Hash the password so plaintext version isn't saved : Default encrytion is pbkdf2.
+    new_user = User(email=email, name=name, password=generate_password_hash(password))
+
+    # add the new user to the database
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/logout')
 def logout():
