9d3575a1e18441585dccfd194503ec68d283aedd
NITIN KUMAR SINGH <nitinitleen1@gmail.com>
2019-08-04 17:34:23 +0530

    login functionality, session persistence added

--- auth.py
+++ auth.py
@@ -1,6 +1,7 @@
 # auth.py
 
 from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask_login import login_user
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from .model import User
@@ -11,9 +12,30 @@ auth = Blueprint('auth', __name__)
 def login():
     return render_template('login.html')
 
+@auth.route('/login', methods=['POST'])
+def login_post():
+    # login code goes here
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+
+    # check if user actually exists
+    # take the user supplied password, hash it, and compare it to the hashed password in database
+    if not user or not check_password_hash(user.password, password): 
+        flash('Please check your login details and try again.')
+        return redirect(url_for('auth.login')) # if user doesn't exist or password is wrong, reload the page
+
+    # if the above check passes, then we know the user has the right credentials
+    login_user(user, remember=remember)
+    return redirect(url_for('main.profile'))
+
+
 @auth.route('/signup')
 def signup():
     return render_template('signup.html')
+
 
 @auth.route('/signup', methods=['POST'])
 def signup_post():
@@ -35,6 +57,8 @@ def signup_post():
     db.session.add(new_user)
     db.session.commit()
     return redirect(url_for('auth.login'))
+
+
 
 @auth.route('/logout')
 def logout():
